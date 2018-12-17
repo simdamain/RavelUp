f9e06625ffd018cac65e652a26b2c25e1b1cd04a
Unknown <simdamain@hotmail.com>
2018-12-18 00:22:33 +0100

    Tache Async
    Debut des taches Async

--- App/app/src/main/java/com/henallux/ravelup/features/connection/LoginActivity.java
+++ App/app/src/main/java/com/henallux/ravelup/features/connection/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.henallux.ravelup.features.connection;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,20 +17,27 @@ import com.henallux.ravelup.features.menus.MainRedirectActivity;
 import com.henallux.ravelup.model.LoginModel;
 import com.henallux.ravelup.model.TokenReceived;
 
+import java.util.ArrayList;
+
 public class LoginActivity extends AppCompatActivity {
-    private ConnectionDAO connectionDAO;
+    //private ConnectionDAO connectionDAO;
+    private TokenReceived tokenReceived;
+    private CheckLogin checkLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        connectionDAO = new ConnectionDAO();
+        checkLogin = new CheckLogin();
+        checkLogin.execute();
+        //connectionDAO = new ConnectionDAO();
 
-        //region logo handling
+        //region logo
         ImageView logo= findViewById(R.id.logo);
         Glide.with(this).load(R.drawable.logo).into(logo);
         //endregion
 
-//        //region errors handling
+//        //region errors
 //        TextInputLayout login = findViewById(R.id.login);
 //        login.setError("mauvais login");
 //        TextInputLayout password = findViewById(R.id.motDePasse);
@@ -42,25 +50,48 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //TODO verify with api
-                LoginModel loginModel = new LoginModel();
+                //TokenReceived tokenReceived;
                 TextInputLayout login = findViewById(R.id.login_LoginActivity);
                 TextInputLayout motDePasse = findViewById(R.id.motDePasse_LoginActivity);
+                LoginModel loginModel = new LoginModel();
+
                 loginModel.setLogin(login.getEditText().getText().toString());
                 loginModel.setMotDePasse(motDePasse.getEditText().getText().toString());
-                TokenReceived tokenReceived = new TokenReceived();
+
                 try {
-                    tokenReceived = connectionDAO.checkLogin(loginModel);
+                    //tokenReceived = connectionDAO.checkLogin(loginModel);
+                    if (tokenReceived.getErrorException().equals("") && tokenReceived.getCode() == 200) {
+                        startActivity(new Intent(LoginActivity.this, MainRedirectActivity.class));
+                    }
 
                 }
                 catch(Exception e){
                         Toast.makeText(LoginActivity.this, "Mauvais Login et mot de passe", Toast.LENGTH_SHORT).show();
                 }
-                if (tokenReceived.getErrorException().equals("") && tokenReceived.getCode() == 200) {
-                    startActivity(new Intent(LoginActivity.this, MainRedirectActivity.class));
-                }
+
 
             }
         });
         //endregion
     }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(checkLogin!=null)
+            checkLogin.cancel(true);
+    }
+    class CheckLogin extends AsyncTask<Void,Void,TokenReceived>{
+        private ConnectionDAO connectionDAO= new ConnectionDAO();
+
+        @Override
+        protected TokenReceived doInBackground(Void... Void){
+            LoginModel loginModel = new LoginModel();
+            TokenReceived tokenReceived = new TokenReceived();
+            try{ tokenReceived= connectionDAO.checkLogin(loginModel);}
+            catch(Exception e){/**Toast*/}
+            return tokenReceived;
+        }
+    }
 }
+
